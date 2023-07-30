6e077f1c60ac6d31d098ff84465bc1bb052cf02c
irishmay29 <81654725+irishmay29@users.noreply.github.com>
2023-07-30 11:35:36 -1000

    modified:   base.css 	modified:   script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// TO DO:
+// Create dropdown animation using javascript 
+
+// function FAQArray {
+//     let comCount = [];
+//     document.getElementById('question');
+// }
+
+// function uploadFAQ {
+//     document.getElementById('show-question') = value('question');
+// }
